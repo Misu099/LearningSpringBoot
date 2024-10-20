b5715ada7670f78424e6bd9acf87cd0b92d6a5cc
Misu099 <mihaialexvp@gmail.com>
2024-10-20 19:57:26 +0300

    .

--- src/main/java/learning_spring_boot/MainController.java
+++ src/main/java/learning_spring_boot/MainController.java
@@ -1,12 +1,16 @@
 package learning_spring_boot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model page) {
+        page.addAttribute("username", "Dorel");
+        page.addAttribute("color", "blue");
         return "home.html";
     }
 }
